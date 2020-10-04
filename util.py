ef419993a85ec0284ff41202020577f40fc02af7
Joseph M.D <djmMax@users.noreply.github.com>
2020-10-05 00:34:53 +0200

    save classifier

--- util.py
+++ util.py
@@ -83,13 +83,14 @@ def set_optimizer(opt, model):
     return optimizer
 
 
-def save_model(model, optimizer, opt, epoch, save_file):
+def save_model(model, optimizer, opt, epoch, save_file, classifier=None):
     print('==> Saving...')
     state = {
         'opt': opt,
         'model': model.state_dict(),
         'optimizer': optimizer.state_dict(),
         'epoch': epoch,
+        'classifier': classifier,
     }
     torch.save(state, save_file)
     del state
